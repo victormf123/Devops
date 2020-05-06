c4ac462871e507c718063c8a5f942e249166eacc
Matheus Freitas <matheus.freitas@ibti.net.br>
2020-05-06 10:53:48 -0300

    nova atualização ingress

--- devops/postegres/postgres_install.sh
+++ devops/postegres/postgres_install.sh
@@ -1,2 +1,3 @@
 #!/bash/sh
-helm install --name postgresql --namespace devops stable/postgresql
+helm install --name nfs-provisioner --namespace devops --set nfs.server=192.168.1.175 --set nfs.path=/data/Kubernetes --set storageClass.defaultClass=true stable/nfs-client-provisioner
+helm install --name postgres --namespace devops --set persistence.existingClaim=postgres-pv-claim stable/postgresql
